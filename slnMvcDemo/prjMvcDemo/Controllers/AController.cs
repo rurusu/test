ef074fd5940f0f4baec1dea6f46e0a224c668e45
rurusu <baintalswife@gmail.com>
2017-08-04 14:35:38 +0800

    0804day

---
+++ slnMvcDemo/prjMvcDemo/Controllers/AController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjMvcDemo.Controllers
+{
+    public class AController : Controller
+    {
+        public ActionResult dataBindingDemo(string name)
+        {
+            DbDemoDataContext db = new DbDemoDataContext();
+            tCustomer t = db.tCustomer.FirstOrDefault(m => m.fName.Contains(name));
+            return View(t);
+        }
+
+        public ActionResult queryByName(string name)
+        {
+            DbDemoDataContext db = new DbDemoDataContext();
+            tCustomer t = db.tCustomer.FirstOrDefault(m => m.fName.Contains(name));
+            if (t != null)
+            {
+                ViewBag.Phone = t.fPhone;
+                ViewBag.Email = t.fEmail;
+                ViewBag.Name = t.fName;
+            }
+                
+            return View();
+        }
+
+
+
+
+        public string getInfoByName(string name)
+        {
+            DbDemoDataContext db = new DbDemoDataContext();
+            tCustomer t = db.tCustomer.FirstOrDefault(m=>m.fName.Contains(name));
+            if(t==null)
+                return "查無資料";
+            return t.fName + "<br/>" + t.fPhone+" / "+t.fEmail;
+        }
+
+        public string sayHello()
+        {
+            return "Hello ASP.NET MVC";
+        }
+        // GET: A
+        public ActionResult m1()
+        {
+            return View();
+        }
+    }
+}
---
+++ slnMvcDemo/prjMvcDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prjMvcDemo.Startup))]
+namespace prjMvcDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
